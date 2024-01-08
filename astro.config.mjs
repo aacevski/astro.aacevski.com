515a88dee1c238e7e17dd3f29890a093c4b7e8f2
Andrej Acevski <andrejacevski@pantone.com>
2024-01-08 21:45:33 +0100

    initial blog commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-import icon from "astro-icon";
+import mdx from '@astrojs/mdx'
+import preact from '@astrojs/preact'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import expressiveCode from 'astro-expressive-code'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  themes: ['github-dark'],
+  shiki: {
+    langs: [],
+  },
+}
+
 export default defineConfig({
-  site: "https://acevski.com",
+  site: 'https://acevski.com',
   integrations: [
-    mdx(),
     sitemap(),
     tailwind(),
     icon(),
     preact({ compat: true }),
+    expressiveCode(astroExpressiveCodeOptions),
+    mdx(),
   ],
-});
+})
